d9b872d34168625d5cd450e09516f7b18cb4f15b
Adnan <adnancse107@gmail.com>
2026-05-01 17:27:53 +0600

    Add new feature in calculator

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ print(datetime.now())
 
 result_add = utils.add(5, 3)
 result_subtract = utils.subtract(20, 2)
+result_multiply = utils.multiply(10, 2)
+result_division = utils.divide(10, 1)
 
 print(f"Addition Result: {result_add}")
 print(f"Subtraction Result: {result_subtract}")
+print(f"Addition Result: {result_multiply}")
+print(f"Subtraction Result: {result_division}")
