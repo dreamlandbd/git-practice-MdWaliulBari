5270ac92559e2e284d659873d5d4672eeb6516fa
Adnan <adnancse107@gmail.com>
2026-05-01 07:34:22 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from datetime import datetime
+import utils
 
 print("Md Waliul Bari")
 print(datetime.now())
+
+result_add = utils.add(5, 3)
+result_subtract = utils.subtract(20, 2)
+
+print(f"Addition Result: {result_add}")
+print(f"Subtraction Result: {result_subtract}")
